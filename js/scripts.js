aabb08b650a6cd111c01fcecb00175cd38ecc994
podolakmateusz <39656669+podolakmateusz@users.noreply.github.com>
2018-07-11 13:33:15 +0200

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,22 @@
 $(document).ready(function() {
     $('.hamburger-icon').click(function() {
-        $('nav').toggleClass('active')})
+        $('nav').toggleClass('active');
+    })
+    
     $('ul li').click(function() {
         $(this).siblings().removeClass('active');
-        $(this).toggleClass('active');})
+        $(this).toggleClass('active');
+    })
+      
+    $('ul .submenu-list li a').click(function() {
+        $('nav').toggleClass('active');
+    })
+    
+    $('ul .menu-list').click(function() {
+        $('nav').toggleClass('active');
+    })
+    
     }
 )
     
+
